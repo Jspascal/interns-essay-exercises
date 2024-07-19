b09b26de4dd6df5694fd86d6b9a6eb4eb4a97ee4
Fyllias <nanguefyllias@gmail.com>
2024-07-19 16:05:02 +0100

    Bank Simulation Project

--- exercises/solutions/Bank_simulation_project_Fyllias.js
+++ exercises/solutions/Bank_simulation_project_Fyllias.js
@@ -1,38 +1,113 @@
+// Class Custermer
+
 class Customer {
-    constructor(name) {
-      this.name = name;
-      this.accounts = [];
-    }
-  
-    addAccount(account) {
-      this.accounts.push(account);
-    }
+  constructor(customerId, name,) {
+    this.name = name;
+    this.customerId = customerId;
+    this.accounts = [];
   }
+
+  getName() {
+    return this.name;
+  }
+
+  getCustomerId() {
+    return this.customerId;
+  }
+
+  addAccount(account) {
+    this.accounts.push(account);
+  }
+}
+
+// this class represents a single node in the linked list
+class CustomerNode {
+  constructor(customer) {
+    this.customer = customer;
+    this.next = null;
+  }
+}
+
+class CustomerList {
+  constructor() {
+    this.node1 = null;  // here, node1 refers to the first node(customer) in the linked list followed by a null value waiting to be filled
+  }
+
+ 
+  addCustomer(customer) {
+    const newNode = new CustomerNode(customer); //added a new customer as newNode
+    newNode.next = this.node1;
+    this.node1 = newNode;
+  }
+
   
-  class Account {
-    constructor(accountNumber, accountType) {
-      this.accountNumber = accountNumber;
-      this.accountType = accountType;
-      this.balance = 0.0;
-    }
-  
-    deposit(amount) {
-      this.balance += amount;
-    }
-  
-    withdraw(amount) {
-      if (amount > this.balance) {
-        throw new Error("Insufficient funds");
+  findCustomerById(customerId) {
+    let current = this.node1;
+    while (current) {
+      if (current.customer.getCustomerId() === customerId) {
+        return current.customer;
       }
-      this.balance -= amount;
+      current = current.next;
+    }
+    return null; 
+  }
+
+  printCustomers() {
+    let current = this.node1;
+    while (current) {
+      console.log(current.customer.getName());
+      current = current.next;
     }
   }
-  
-  class Transaction {
-    constructor(accountNumber, transactionType, amount) {
-      this.accountNumber = accountNumber;
-      this.transactionType = transactionType;
-      this.amount = amount;
-    }
+}
+
+
+// Class Account
+class Account {
+  constructor(accountNumber) {
+    this.accountNumber = accountNumber;
+    this.balance = 0.0;
+    this.transactions = [];
   }
-  
+
+  getAccountNumber() {
+    return this.accountNumber;
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+    this.transactions.push(new Transaction(amount, "Deposit"));
+  }
+
+  withdraw(amount) {
+    if (amount > this.balance) {
+      throw new Error("Insufficient funds for withdrawal");
+    }
+    this.balance -= amount;
+    this.transactions.push(new Transaction(amount, "Withdrawal"));
+  }
+
+  getTransactions() {
+    return this.transactions;
+  }
+}
+
+class Transaction {
+  constructor(amount, type) {
+    this.amount = amount;
+    this.type = type;
+  }
+
+  getAmount() {
+    return this.amount;
+  }
+
+  getType() {
+    return this.type;
+  }
+}
+    
